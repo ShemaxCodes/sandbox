fef6e1d98d279e5a0aa86480159b793c13ca5a28
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-03-16 15:20:26 -0400

    Create age.js

---
+++ age.js
@@ -0,0 +1,9 @@
+let age = 20
+
+if(age < 16){
+    console.log("You cannot drive")
+}else if(age < 18){
+    console.log("can't hate from outside the club")
+}else if(age <= 21){
+    console.log("you cannot drink")
+}
